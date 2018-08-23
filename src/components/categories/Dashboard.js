8922436c17d19b3b6642f8dc02ef90abce8b2b2e
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-22 18:07:31 -0700

    dashboard added

---
+++ src/components/categories/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Categories from './Categories';
+import CategoryForm from './CategoryForm';
+import { load, add, update } from './actions';
+
+class CategoriesContainer extends Component {
+
+  static propTypes = {
+    categories: PropTypes.array,
+    load: PropTypes.func.isRequired,
+    add: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.load();
+  }
+
+  render() {
+    const { categories, add } = this.props;
+
+    return (
+      <div>
+        <section>
+          <h3>Add an Category</h3>
+          <CategoryForm onComplete={add}/>
+        </section>
+
+        {categories && 
+          <section>
+            <h3>Categories</h3>
+            <Categories
+              categories={categories}
+              onUpdate={update}
+            />
+          </section>
+        }
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    categories: state
+  }),
+  { load, add }
+)(CategoriesContainer);
