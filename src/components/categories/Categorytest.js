06f4c490c5a72264f3c99a788d230f54edbce56b
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-23 22:58:08 -0700

    Category test passes

---
+++ src/components/categories/Categorytest.js
@@ -0,0 +1,32 @@
+import Category from './Category';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('Category', () => {
+
+  it('renders display or edit', () => {
+    const handleRemove = jest.fn();
+    const handleUpdate = jest.fn();
+
+    const category = { key: 'abc', name: 'shopping', budget: '400' };
+    const wrapper = shallow(<Category
+      category={category}
+      onRemove={handleRemove}
+      onUpdate={handleUpdate}
+    />);
+
+    const component = wrapper.instance();
+
+    expect(wrapper.state('editing')).toBe(false);
+    component.handleEdit();
+    expect(wrapper.state('editing')).toBe(true);
+    component.handleEndEdit();
+    expect(wrapper.state('editing')).toBe(false);
+
+    component.handleDelete();
+
+    const removeCalls = handleRemove.mock.calls;
+    expect(removeCalls.length).toBe(1);
+    expect(removeCalls[0][0]).toBe(category.key);
+  });
+});
