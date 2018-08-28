f9fce5316f133a686687974ddf47cfa603ea07aa
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-27 22:34:40 -0700

    error file added, home page displays

---
+++ src/components/app/Error.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getError } from './error-reducers';
+import { clearError } from './error-actions';
+import styles from './Error.css';
+
+export class Error extends Component {
+
+  static propTypes = {
+    error: PropTypes.any,
+    clearError: PropTypes.func
+  };
+  
+  componentDidUpdate() {
+    const { error, clearError } = this.props;
+
+    if(error) {
+      setTimeout(() => {
+        clearError();
+      }, 6000);
+    }
+  }
+
+  render() {
+    const { error } = this.props;
+    if(!error) return null;
+
+    return (
+      <pre className={styles.error}>{error}</pre>
+    );
+  }
+}
+export default connect(
+  state => ({
+    error: getError(state)
+  }),
+  { clearError }
+)(Error);
